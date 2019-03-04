d559b853b2a53a36f0fe789cf1e09cd93e07ab71
Nikotos <bup1350@gmail.com>
2019-03-04 21:04:47 +0300

    Initial Commit

---
+++ autograd/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  autograd
+//
+//  Created by Oak Nick on 04/03/2019.
+//  Copyright © 2019 Oak Nick. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
